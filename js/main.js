453e4e7807d484d744988f1c5c587e6aa188775f
kballant <kevin.ballantyne@gmail.com>
2019-06-08 12:02:45 -0400

    modified:   _includes/nav.html 	modified:   _layouts/main.html 	modified:   css/main.css 	modified:   css/ol.css 	new file:   js/main.js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/* author:		Kevin Ballantyne
+	updated:		20190606
+	version:		1.0
+*/
+
+(function($){
+	$(document).ready(function(){
+		$('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+			event.preventDefault(); 
+			event.stopPropagation(); 
+			$(this).parent().siblings().removeClass('open');
+			$(this).parent().toggleClass('open');
+		});
+	});
+})(jQuery);
